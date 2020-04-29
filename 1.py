052c7dbccdf0780b87956481343818e057c3dba1
utkrisht <utkrishtsharma93@gmail.com>
2020-04-29 08:12:28 +0000

    First commit

---
+++ 1.py
@@ -0,0 +1,5 @@
+import finalmodule1 as s
+print("program startin btch .....")
+print(s.sentiment("This movie was awesome! The acting was great, plot was wonderful, and there were pythons...so yea!"))
+print(s.sentiment("This movie was utter junk. There were absolutely 0 pythons. I don't see what the point was at all. Horrible movie, 0/10"))
+print(" yo yo yo øprogram ended  btch .....")
